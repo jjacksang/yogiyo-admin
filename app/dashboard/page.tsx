11a4d171453db98f414c0061d7de84d8dd47cb88
kinnatzzang <93966256+kinnatzzang@users.noreply.github.com>
2024-02-25 16:28:39 +0900

    [sidebar] 대시보드 사이드바 작업중

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,17 +1,24 @@
 import Link from "next/link";
 import React from "react";
 import DashboardNavbar from "@/components/common/DashboardNavbar";
-
+import DashboardSidebar from "@/components/common/DashboardSidebar";
 
 type Props = {}
 
-const page = (props: Props) => {
+const Page = (props: Props) => {
   return (
-    <>
-     <DashboardNavbar/>   
-    </>
-  )
-}
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <DashboardNavbar />
+      <div style={{ display: 'flex', flexDirection: 'row', height: '100vh' }}>
+        <DashboardSidebar />
+        <div style={{ flex: 1 }}> {/* 메인 컨텐츠 영역 */}
+          {/* 여기에 페이지의 주요 내용 */}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default page
+
+export default Page
 
--- components/common/DashboardSidebar.tsx
+++ components/common/DashboardSidebar.tsx
@@ -3,9 +3,11 @@ import React from 'react'
 type Props = {}
 
 const DashboardSidebar = (props: Props) => {
-  return (
-    <div></div>
-  )
-}
+    return (
+      <div style={{ width: '260px', height: '100%', background: '#f0f2f5' }}> {/* 여기에 배경색 등 스타일 추가 가능 */}
+        
+      </div>
+    );
+  };
 
 export default DashboardSidebar
