35b77625cb1fbd8e6a1f3adbea9bd95c0ca27ac8
kinnatzzang <93966256+kinnatzzang@users.noreply.github.com>
2024-02-28 21:34:26 +0900

    병합작업중

--- app/services/loginAPI.ts
+++ app/services/loginAPI.ts
@@ -1,6 +1,7 @@
+import { SocialLogin } from "@/lib/types";
 import axios from "axios";
 
-const getAxios = axios.create({
+export const getAxios = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
 });
 
@@ -13,7 +14,7 @@ export const emailJoin = async (email: string, password: string, nickname: strin
     };
     console.log(userData);
     try {
-        const resJoin = await getAxios.post("/member/join", userData);
+        const resJoin = await getAxios.post("/owner/join", userData);
         return resJoin.data;
     } catch (error) {
         console.log(error);
@@ -28,16 +29,51 @@ export const emailLogin = async (email: string, password: string) => {
         providerType: "DEFAULT",
     };
     console.log(userData);
-    const resSubmit = await getAxios.post("/member/login", userData);
+
+    const resSubmit = await getAxios.post("/owner/login", userData);
     if (resSubmit.status >= 200 && resSubmit.status < 300) {
-        // window.location.href = "/";
         console.log(`${resSubmit.data.userId} 로그인 성공`);
         console.log(resSubmit);
+
+        const userId = resSubmit.data.userId;
+        const userEmail = resSubmit.data.email;
+
+        // document.cookie = `userId=${userId}; path=/`;
+        // document.cookie = `email=${userEmail}; path=/`;
+
+        // window.location.href = "/";
+        return { userId, userEmail };
     } else {
         console.log("Login api error");
+        return null;
     }
 };
 
-// export const SocialKakao = async (req: ReqAuth) => {
-//     const baseURL = "https://kauth.kakao.com/authorize";
-// };
+export const getCookie = async (reqbody: SocialLogin) => {
+    const headers = {
+        "Content-Type": "application/json",
+    };
+    const config = { headers };
+    const resTokenAPI = await getAxios.post("owner/login", reqbody, { withCredentials: true });
+
+    const isLoggedIn = true;
+    const userId = resTokenAPI.data.userId;
+    const email = resTokenAPI.data.email;
+
+    console.log(resTokenAPI.data.userId);
+    console.log(resTokenAPI.data.email);
+
+    return { userId, email, isLoggedIn };
+};
+
+export const KakaoLogin = () => {
+    //const setLogin = useSetRecoilState(loginState);
+    const CLIENT_ID = `${process.env.NEXT_PUBLIC_KAKAO_API_KEY}`;
+    const REDIRECT_URI = `${process.env.NEXT_PUBLIC_KAKAO_LOGIN_REDIRECT_URI}`;
+    const RESPONSE_TYPE = "code";
+    const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`;
+    window.location.href = KAKAO_AUTH_URL;
+
+    const url = new URL(window.location.href);
+    const authorizationCode = url.searchParams.get("code");
+};
