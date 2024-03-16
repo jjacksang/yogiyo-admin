df54366a9040192c4abdfd18de7eb2eac4cd4761
Sangwoo <jswoo1995@gmail.com>
2024-03-16 21:24:13 +0900

    닉네임 변경 가라디자인

--- app/my/page.tsx
+++ app/my/page.tsx
@@ -4,8 +4,10 @@ import React, { useState } from "react";
 import { useRecoilState } from "recoil";
 import { userStateAtom } from "../recoil/state";
 import { getAxios } from "../services/loginAPI";
+import { useRouter } from "next/navigation";
 
 export default function NickChange() {
+    const router = useRouter();
     const [userNickname, setUserNickname] = useRecoilState(userStateAtom);
     const [newNickname, setNewNickname] = useState("");
 
@@ -28,23 +30,32 @@ export default function NickChange() {
                     return prev;
                 });
             }
+            router.push("/");
         } catch (error) {
             console.error("닉네임 변경 중 오류가 발생", error);
         }
     };
     console.log(userNickname);
     return (
-        <div>
-            <p>닉네임 변경하기</p>
-            {userNickname && <p>현재 닉네임: {userNickname.nickname}</p>}
-            <div>
-                <input
-                    placeholder="변경할 닉네임 적어라"
-                    value={newNickname}
-                    type="text"
-                    onChange={handleChange}
-                ></input>
-                <button onClick={submitNickname}>닉네임 변경</button>
+        <div className="flex flex-col p-10 m-auto mt-12">
+            <p className="flex justify-center mb-4 text-xl font-bold">닉네임 변경하기</p>
+            <div className="flex flex-col items-center border rounded-lg gap-4">
+                {userNickname && <p className="mt-5">현재 닉네임: {userNickname.nickname}</p>}
+                <div className="flex px-6 py-4">
+                    <input
+                        placeholder="변경할 닉네임 적어라"
+                        value={newNickname}
+                        type="text"
+                        onChange={handleChange}
+                        className="border rounded pl-3 py-1.5"
+                    ></input>
+                    <button
+                        onClick={submitNickname}
+                        className="border rounded-lg px-2 ml-4 cursor-pointer"
+                    >
+                        닉네임 변경
+                    </button>
+                </div>
             </div>
         </div>
     );
