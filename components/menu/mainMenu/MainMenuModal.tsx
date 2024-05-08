8850f9da71d5420d43393484470b20b4ee359871
Sangwoo <jswoo1995@gmail.com>
2024-05-09 00:20:06 +0900

    대표 메뉴 설정 api호출 성공 및 전체 조회시 데이터 확인

--- components/menu/mainMenu/MainMenuModal.tsx
+++ components/menu/mainMenu/MainMenuModal.tsx
@@ -1,23 +1,22 @@
 "use client";
 import { useRecoilValue } from "recoil";
 import { menuItemAtom, shopIdAtom } from "../../../app/recoil/state";
-import React from "react";
+import React, { useState } from "react";
 import { MenuItem, MenusItem, ModalProps } from "@/lib/types";
 import { getAxios } from "@/app/services/loginAPI";
 
 export default function MainMenuModal({ onClose }: ModalProps) {
     const menuGroup = useRecoilValue(menuItemAtom);
     const shopId = useRecoilValue(shopIdAtom);
-    console.log(shopId);
-    console.log(menuGroup);
-    const groupId = menuGroup.map((item) => item.id);
-    console.log(groupId);
-    if (!menuGroup) return null;
+    const [checkMenuList, setCheckMenuList] = useState<number[]>([]);
+    const [isChecked, setIsChecked] = useState([]);
+    const [menuIds, setMenuIds] = useState<number>();
+
     const setMainMenu = async () => {
         try {
             const res = await getAxios.put("/owner/signature-menu/set", {
                 shopId: shopId,
-                menuIds: groupId,
+                menuIds: checkMenuList,
             });
             if (res.status === 204) {
                 console.log(res);
@@ -29,12 +28,19 @@ export default function MainMenuModal({ onClose }: ModalProps) {
             console.error(error);
         }
     };
+    const onCheckedList = (checked: boolean, ids: number) => {
+        if (checked) {
+            setCheckMenuList([...checkMenuList, ids]);
+        } else if (!checked) {
+            setCheckMenuList(checkMenuList.filter((el) => el !== ids));
+        }
+    };
 
-    console.log(menuGroup);
+    console.log(checkMenuList);
 
     return (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-            <div className="flex flex-col bg-white w-1/2 h-auto rounded-2xl my-20">
+            <div className="flex flex-col bg-white w-1/2 h-3/4 rounded-2xl my-20 overflow-y-auto">
                 <div className="flex py-4">
                     <p className="flex justify-center text-xl font-bold w-full">대표메뉴 설정</p>
                     <button className="px-4" onClick={onClose}>
@@ -48,7 +54,7 @@ export default function MainMenuModal({ onClose }: ModalProps) {
                             placeholder="메뉴명"
                         ></input>{" "}
                         {/* 메뉴 검색 */}
-                        <ul className="text-sm text-custom-gray list-disc">
+                        <ul className="text-sm text-custom-gray list-disc px-5">
                             <li>대표메뉴를 선택(클릭)한 순서대로 저장됩니다.</li>
                             <li>
                                 대표메뉴는 사진이 등록된 메뉴만 등록 가능합니다.(최대 6개까지, 주류
@@ -56,37 +62,57 @@ export default function MainMenuModal({ onClose }: ModalProps) {
                             </li>
                         </ul>
                     </div>
-                    <div>
+                    <div className="px-4">
                         {/* 대표메뉴 그룹 조건렌더 영역 */}
                         {menuGroup?.map((menuItem) => (
                             <div
-                                className="flex flex-col px-8 py-4 mt-8 border rounded-lg bg-white"
+                                className="flex flex-col px-8 py-4 mt-4 border rounded-lg "
                                 key={menuItem.id}
                             >
-                                <div
-                                    className="flex justify-between w-full mb-4"
-                                    // onClick={(e) => {
-                                    //     e.preventDefault();
-                                    //     setSelectGroupId(menuItem.id);
-                                    // }}
-                                >
+                                <div className="flex justify-between w-full mb-2">
                                     <div className="gap-2">
-                                        <p className="text-base font-bold text-font-black">
+                                        <p className="text-xl font-bold text-font-black">
                                             {menuItem.name}
                                         </p>
                                     </div>
                                     {/* 판매, 품절 등 드롭다운 메뉴 */}
                                 </div>
-                                {menuItem?.menus?.map((item) => (
-                                    <div className="flex justify-between w-full mb-4" key={item.id}>
+                                {menuItem.menus?.map((item) => (
+                                    <div
+                                        className="flex justify-between w-full mb-4 px-2"
+                                        key={item.id}
+                                    >
                                         <div className="flex flex-col">
-                                            <span className="text-base font-bold">{item.name}</span>
-                                            <p className="text-xs">{item.price}</p>
+                                            <div className="flex">
+                                                <input
+                                                    type="checkbox"
+                                                    value={item.id}
+                                                    className="text-base font-bold mr-2"
+                                                    checked={
+                                                        checkMenuList.includes(item.id)
+                                                            ? true
+                                                            : false
+                                                    }
+                                                    onChange={(e) => {
+                                                        onCheckedList(
+                                                            e.target.checked,
+                                                            parseInt(e.target.value)
+                                                        );
+                                                    }}
+                                                />
+                                                <label>{item.name}</label>
+                                            </div>
+                                            <p className="text-xs text-custom-gray pl-[22px]">
+                                                {item.price}원
+                                            </p>
                                         </div>
                                     </div>
                                 ))}
                             </div>
                         ))}
+                        <div>
+                            <button onClick={setMainMenu}>저장</button>
+                        </div>
                     </div>
                 </div>
             </div>
