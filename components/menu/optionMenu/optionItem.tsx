2d9438e67bbfd409e9005e9ccf604fbf208ad504
Sangwoo <jswoo1995@gmail.com>
2024-05-14 01:45:30 +0900

    optionItem 컴포넌트 분리중인 파일 실수로 푸시함

--- components/menu/optionMenu/optionItem.tsx
+++ components/menu/optionMenu/optionItem.tsx
@@ -1,7 +1,7 @@
 const ItemComponent = ({}) => {
     return (
         <div>
-            <div className="flex justify-between w-full mb-4" key={}>
+            <div className="flex justify-between w-full mb-4">
                 <div className="flex flex-col">
                     <span className="text-base font-bold">{}</span>
                     <p className="text-xs text-custom-gray pb-2">{}</p>
@@ -31,18 +31,7 @@ const ItemComponent = ({}) => {
                     </div>
                 </div>
             </div>
-            <div className="flex border-t py-2">
-                <span className="text-xs px-2 text-yogiyo-blue">옵션추가</span>
-                <span className="text-xs px-2">옵션 순서변경</span>
-            </div>
-            <div className="flex items-between text-sm">
-                <span>유형</span>
-                <span className="">필수옵션 필수 1개 선택 옵션설정</span>
-            </div>
-            <div className="text-sm">
-                <span>연결메뉴</span>
-                <span>리코타치즈샐러드, 연어샐러드, 닭가슴살샐러드 메뉴연결</span>
-            </div>{" "}
+
             {/* 옵션 그룹 밑에 있는 설명란 임시 저장 */}
         </div>
     );
