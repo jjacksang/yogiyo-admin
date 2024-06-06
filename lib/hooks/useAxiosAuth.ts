0716a5034122ed0e4e5e87084257fa0d2d3c45b9
Sangwoo <jswoo1995@gmail.com>
2024-06-06 22:40:41 +0900

    refreshToken 동작, error handler 작업 중

---
+++ lib/hooks/useAxiosAuth.ts
@@ -0,0 +1,42 @@
+"use client";
+import { getAxios } from "@/app/services/loginAPI";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+import { useRefreshToken } from "./useRefreshToken";
+
+const useAxiosAuth = () => {
+    const { data: session } = useSession();
+    const refreshToken = useRefreshToken();
+
+    useEffect(() => {
+        const requestIntercept = getAxios.interceptors.request.use(
+            (config) => {
+                if (!config.headers["Authorization"]) {
+                    config.headers["Authorization"] = `Bearer ${session?.user.accessToken}`;
+                }
+                return config;
+            },
+            (error) => Promise.reject(error)
+        );
+        const responseIntercept = getAxios.interceptors.response.use(
+            (response) => response,
+            async (error) => {
+                const prevRequest = error.config;
+                if (error.response.status === 401 && !prevRequest.sent) {
+                    prevRequest.sent = true;
+                    await refreshToken();
+                    prevRequest.headers["Authorizetion"] = `Bearer ${session?.user.accessToken}`;
+                    return getAxios(prevRequest);
+                }
+                return Promise.reject(error);
+            }
+        );
+        return () => {
+            getAxios.interceptors.request.eject(requestIntercept);
+            getAxios.interceptors.response.eject(responseIntercept);
+        };
+    }, [session]);
+    return getAxios;
+};
+
+export default useAxiosAuth;
