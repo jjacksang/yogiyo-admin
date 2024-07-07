65aed57daa191c985ef1eab644ae1da40a774c63
Sangwoo <jswoo1995@gmail.com>
2024-07-07 22:18:22 +0900

    [fix] 지저분한 sidebar UI변경

--- components/menuSidebar/ReviewTab.tsx
+++ components/menuSidebar/ReviewTab.tsx
@@ -15,26 +15,22 @@ export const ReviewTab = () => {
     };
 
     return (
-        <>
-            <a
-                style={{ lineHeight: "16px", color: "rgba(0, 0, 0, 0.4)" }}
-                className="pl-3 text-xs font-bold flex flex-row items-center h-10 decoration-none"
-            >
-                <svg
-                    width="20"
-                    height="20"
-                    fill="none"
-                    style={{ fill: "none" }}
-                    viewBox="0 0 24 24"
-                    xmlns="http://www.w3.org/2000/svg"
-                ></svg>
-                리뷰
-                <button onClick={OpenCloseBtn}>ㅇ</button>
+        <div className="ml-5 font-bold text-swiper-num cursor-pointer" onClick={OpenCloseBtn}>
+            <a className="pl-3 flex flex-row justify-between items-center h-10 decoration-none">
+                <span className="text-sm">리뷰</span>
+                <button>
+                    <svg width="20" height="20" viewBox="0 0 24 24" className="mr-2">
+                        <path
+                            fill="rgba(0,0,0,0.4)"
+                            d="M7.41 8.58L12 13.17l4.59-4.59L18 10l-6 6-6-6z"
+                        />
+                    </svg>
+                </button>
             </a>
             {showOption ? (
-                <ul>
+                <ul className="">
                     <li
-                        className="text-base pt-2 pl-6"
+                        className="text-sm pt-2 pl-6"
                         onClick={() => handleMenuClick("ReviewManagement")}
                     >
                         리뷰 관리
@@ -43,6 +39,6 @@ export const ReviewTab = () => {
             ) : (
                 <div></div>
             )}
-        </>
+        </div>
     );
 };
