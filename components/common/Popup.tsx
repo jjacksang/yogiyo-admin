2c769d1f7478ba136c9e57f3a7fac656e02b468a
Sangwoo <jswoo1995@gmail.com>
2024-07-31 16:34:53 +0900

    [feat] login 실패 시 Popup모달 설정

--- components/common/Popup.tsx
+++ components/common/Popup.tsx
@@ -12,7 +12,7 @@ const Popup = ({ message, onClose }: IPopup) => {
     return (
         <div className="flex justify-center items-center fixed inset-0 bg-black bg-opacity-50 ">
             <div className="flex flex-col justify-center items-center border rounded-lg w-96 h-36 bg-white py-4 relative shadow-lg shadow-custom-gray">
-                <span className="flex items-center gap-2 text-lg pb-2 h-full">
+                <span className="flex items-center gap-2 text-lg pb-2 h-full ">
                     <TbAlertSquareRounded size={"32px"} />
                     {message}
                 </span>
